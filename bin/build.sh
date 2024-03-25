c9367b13bbc2cb287abe19b68e880ee149c39da4
Steven Miers <steven.miers@gmail.com>
2024-03-25 02:57:37 -0500

    Regen docs.

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ${BASH_SOURCE%/*}/clean.sh
 # Run in browser:
-poetry run pdoc csv_batcher --logo https://raw.githubusercontent.com/tangledpath/csv-batcher/master/csv_batcher.png --favicon https://raw.githubusercontent.com/tangledpath/csv-batcher/master/csv_batcher_sm.png
+#poetry run pdoc csv_batcher --logo https://raw.githubusercontent.com/tangledpath/csv-batcher/master/csv_batcher.png --favicon https://raw.githubusercontent.com/tangledpath/csv-batcher/master/csv_batcher_sm.png
 poetry run pdoc csv_batcher -o ./docs --logo https://raw.githubusercontent.com/tangledpath/csv-batcher/master/csv_batcher.png --favicon https://raw.githubusercontent.com/tangledpath/csv-batcher/master/csv_batcher_sm.png
 poetry build
